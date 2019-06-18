c59f38c3c616080780ecbd62e4b8a0cf7d59b157
Jose Angel Sanchez Castillejos <jsanchezcastillejos@gmail.com>
2019-06-17 20:44:25 -0500

    Improve solution

--- Arcade/ListBackwoods/SwapDiagonals.py
+++ Arcade/ListBackwoods/SwapDiagonals.py
@@ -36,11 +36,6 @@ Input/Output
     
 def swapDiagonals(matrix):
     size = len(matrix)
-    for i in range(0,size/2):
-        temp = matrix[i][i]
-        matrix[i][i] = matrix[i][size-i-1]
-        matrix[i][size-i-1] = temp
-        temp = matrix[size-i-1][i]
-        matrix[size-i-1][i] = matrix[size-i-1][size-i-1]
-        matrix[size-i-1][size-i-1] = temp
+    for i in range(0, size):
+        matrix[i][i],  matrix[i][size -i -1] = matrix[i][size -i -1], matrix[i][i]
     return matrix
